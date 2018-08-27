e5a2cc5605e7a5f367fc6efb19dcf379200b354b
Samuel Gongora-Garcia <s.gongoragarcia@gmail.com>
2018-08-27 15:29:21 +0200

    b_image filter improved

--- pipeline/scamp_filter_elvis.py
+++ pipeline/scamp_filter_elvis.py
@@ -527,7 +527,7 @@ class ScampFilterELViS:  # TODO Split scamp_filter method into single methods
             pm = float(o_df['PM'])
             class_star = float(o_df['MEAN_CLASS_STAR'])
 
-            if pm < 0.6 and class_star < 0.7:
+            if pm < 1.0 and class_star < 0.6:
                 rejected.append(source_)
             else:
                 accepted.append(source_)
